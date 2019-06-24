fb701d00c3701fb00d5c84d82c15485deef559ec
Njeri <njericooper@gmail.com>
2019-06-24 13:43:51 -0500

    update

--- Product-Form/DataTable.js
+++ Product-Form/DataTable.js
@@ -1,5 +1,16 @@
 function table() {
-    row1:  someShit; 
+    if (("`${table}` tbody").length == 0){
+        `${table}`.append("<tbody></tbody>");
+    }
+    ("`${table}` tbody").append(
+        "<tr>" + "<td>Sometext about object 1</td>"
+    )
+
+    row1: someShit; 
     row2: someShit; 
     row3: someShit;
+}
+
+function tableUpdate() {
+    if ( $("#object1"))
 }
