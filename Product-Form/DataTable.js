0a843c81eb6f8dcf895e2bd5b0b0f207dbe1d3b9
Njeri <njericooper@gmail.com>
2019-06-24 19:39:59 -0500

    eh

--- Product-Form/DataTable.js
+++ Product-Form/DataTable.js
@@ -1,16 +1,35 @@
-function table() {
-    if (("`${table}` tbody").length == 0){
-        `${table}`.append("<tbody></tbody>");
+ function tableUpdate() {
+    var table = document.getElementById("table");
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    cell1.innerHTML = document.getElementById("input").value;
+    cell2.innerHTML = document.getElementById("input2").value;
+    cell3.innerHTML = document.getElementById("input3").value;
+    //var items = [];
+    //items.push(cell1.innerHTML, cell2.innerHTML, cell3.innerHTML);
+    //localStorage.setItem(row, JSON.stringify(items));
+    //storedItems = JSON.parse(localStorage.getItem(item));
+    //return storedItems;
+    
+    var theTable = document.getElementById('table');
+    for (var r = 0, n = theTable.rows.length; r < n; r++) {
+        for (var c = 0, m = theTable.rows[r].cells.length; c < m; c++) {
+            const items = [];
+            items.push(theTable.rows[r].cells[c].innerHTML);
+            localStorage.setItem(JSON.stringify(items));
+            storedItems = JSON.parse(localStorage.getItem(items));
+            var tableContents = document.getElementById("output").innerHTML = items;
+            alert(tableContents);
+        }
+        
     }
-    ("`${table}` tbody").append(
-        "<tr>" + "<td>Sometext about object 1</td>"
-    )
-
-    row1: someShit; 
-    row2: someShit; 
-    row3: someShit;
+   
+   
 }
 
-function tableUpdate() {
-    if ( $("#object1"))
-}
+
+
+
+    
