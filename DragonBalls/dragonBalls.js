9be30df293baf6cb36bbb017c4fc60c6fbbff441
Njeri <njericooper@gmail.com>
2019-06-26 12:56:22 -0500

    Integer Counting Loop

---
+++ DragonBalls/dragonBalls.js
@@ -0,0 +1,14 @@
+function dragonBall(){ 
+    var output1 = "The Journey continues...Next time on Dragon Ball Z!";
+    for(let i = 0; i<=7; i++) {
+        if(i<6)
+        
+            console.log(" The Journey continues...Next time on Dragon Ball Z!");
+            
+            
+        else if (i=7)
+            console.log(" Summoning Shenron");
+    }
+}
+
+dragonBall();
