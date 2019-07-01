67878804913a32400ea3ff1ac20a4d94603bcd0a
Njeri <njericooper@gmail.com>
2019-07-01 09:25:42 -0500

    Return String index if string is n length

---
+++ ScriptString/scriptString.js
@@ -0,0 +1,11 @@
+function indexString() { 
+
+var string = "script";
+var index = 4;
+
+if(string.length >= 5){
+    console.log("the character at index " + index + " is " + string.charAt(index));
+    }
+}
+
+indexString();
