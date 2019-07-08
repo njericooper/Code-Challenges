c9b7195e492c762537d3bb09c8691361d79d23f2
Njeri <njericooper@gmail.com>
2019-07-08 17:28:47 -0500

    onClick and addEventListener Code Challenge

---
+++ onClick/onClick.js
@@ -0,0 +1,12 @@
+function clicky1() {
+    document.getElementById("button-1-output").innerHTML = "1";
+}
+
+function clicky2() {
+    document.getElementById("button-2-output").innerHTML = "2";
+}
+
+function clickClack() {
+    document.getElementById("pad-output").innerHTML = "3";
+}
+
